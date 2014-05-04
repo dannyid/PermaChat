7ba1dd9d6c48dfdb92bfeee7b5bf8054c6751b88
Danny Shekhtman <danielshekhtman@gmail.com>
2014-05-04 19:23:04 -0400

    updating manifest

--- extension/background.js
+++ extension/background.js
@@ -16,6 +16,17 @@ socket.on("connect", function(data) {
 
     // join chatroom: roomName
     socket.emit( "join", roomName);
+
+    var $chatDiv = $('<div id = chatDiv></div>');
+    $chatDiv
+      .css('position','absolute')
+      .css('width', '100%')
+      .css('bottom','0')
+      .css('left','0')
+      .css('z-index', '99999999')
+      .css('hrigh','150px')
+    $chatDiv.text('hello')
+    $('body').append($chatDiv)
   };
 
   chrome.browserAction.onClicked.addListener(onClick);
