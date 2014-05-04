7ee062ba504f8b02f76616e6986cb2bdfde38c09
Danny Shekhtman <danielshekhtman@gmail.com>
2014-05-04 18:55:01 -0400

    git is annoying

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 var socket = io.connect('http://localhost:3000/');
 
 socket.on("connect", function(data) {
@@ -21,21 +20,3 @@ socket.on("connect", function(data) {
 
   chrome.browserAction.onClicked.addListener(onClick);
 });
-
-=======
-function setIcon(path) {
-  chrome.browserAction.setIcon({"path":path})
-};
-
-function onClick(tab) {
-  var parser = document.createElement('a')
-  parser.href = tab.url;
-  
-  var roomName = parser.hostname + parser.pathname;
-
-  // open websocket and join chatroom: roomName
-  //
-};
-
-chrome.browserAction.onClicked.addListener(onClick);
->>>>>>> 28e1428ec98158bf6d9371883d2e596105ac8713
