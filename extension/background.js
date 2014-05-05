5c2fe2b8f156de88cd493e49769a895cc0220309
Danny Shekhtman <danielshekhtman@gmail.com>
2014-05-04 22:04:19 -0400

    moving permchatDiv css to file

--- extension/background.js
+++ extension/background.js
@@ -2,20 +2,19 @@
 var socket = io.connect('http://localhost:3000/');
 
 socket.on("connect", function(data) {
-  function setIcon(path) {
-    chrome.browserAction.setIcon({"path":path})
-  };
-
   function onClick(tab) {
     var parser = document.createElement('a')
     parser.href = tab.url;
-  
+ 
+    // room name = url of current page -- needs parsing work
     var roomName = parser.hostname + parser.pathname;
 
     // join chatroom: roomName
-    socket.emit( "join", roomName);
+    socket.emit("join", roomName);
 
-    chrome.tabs.executeScript(tab.id, {'file': 'injectChat.js'}, function(data){console.log(tab.id)})
+    // on browserAction click, inject chatbox js and css
+    chrome.tabs.executeScript(tab.id, {'file': 'injectChat.css'}, function(data){'chatbox css injected'})
+    chrome.tabs.executeScript(tab.id, {'file': 'injectChat.js'}, function(data){'chatbox injected with js'})
   };
 
   chrome.browserAction.onClicked.addListener(onClick);
--- extension/injectChat.js
+++ extension/injectChat.js
@@ -4,11 +4,11 @@ var html = document.getElementsByTagName('html')[0];
 
 if(document.getElementById('permachatDiv')){
   body.removeChild(permachatDiv);
-  html.style.paddingBottom = '0px';
+  html.style.marginBottom = '0px';
 } else {
   var permachatDiv = document.createElement('div');
   permachatDiv.id = 'permachatDiv';
   permachatDiv.setAttribute('style','position:fixed; display:block; background-color:grey; width:100%; height:250px; bottom:0; left:0; z-index:99999999;');
-  html.style.paddingBottom = '250px';
+  html.style.marginBottom = '250px';// this doesn't seem to work all the time -- see w3schools
   body.appendChild(permachatDiv);
 };
