13737229a2d26da03be470ef930952933dd38ead
Danny Shekhtman <danielshekhtman@gmail.com>
2014-05-19 15:16:19 -0400

    it's working i guess

--- server/public/js/script.js
+++ server/public/js/script.js
@@ -3,14 +3,13 @@ var socket = io.connect();
 
 var nickname = '';
 
-function addMessage( msg, nick ) {
-	$( "#chatEntries" ).append( '<div class="message"><p><span id="nick">'+nick+'</span> : '+msg+'</p></div>' );
+function addMessage( message, nick ) {
+	$( "#chatEntries" ).append( '<div class="message"><p><span id="nick">'+nick+'</span> : '+message+'</p></div>' );
 };
 
 function sendMessage() {
-	if ( $( '#messageInput' ).val() != "" ) 
-	{
-		socket.emit( 'message', $( '#messageInput' ).val());
+	if ( $( '#messageInput' ).val() != "" ) {
+		socket.emit( 'message', {"nick": nickname, "message": $( '#messageInput' ).val()} );
 		addMessage( $( '#messageInput' ).val(), nickname + " (Me)");
 		$( '#messageInput' ).val('');
 	}
@@ -29,7 +28,7 @@ function setNick() {
 
 // join room specified by URL
 socket.on( "connect", function( data ) {
-    socket.emit( "joinRoom", location.pathname.slice( 1 ) );
+    socket.emit( "join", location.pathname.slice( 6 ) );
 });
 
 // add message to screen upon receiving it from server
@@ -37,29 +36,23 @@ socket.on( 'message', function( data ) {
 	addMessage( data[ 'message' ], data[ 'nick' ] );
 });
 
-
 $( document ).ready( function() {
-    if (nickname) {
-        $( "#nickInput" ).val(nickname); 
-        console.log($( "#nickInput" ).val());
-    };    
-
     // initialize fn - hide chat box until nick is set, add events to buttons
     $( function() {
 	    $( "#chatControls" ).hide();
-        $( 'input' ).focus(); // focus on nick input upon page load
+        $( '#nickInput' ).focus(); // focus on nick input upon page load
 	    $( "#nickSet" ).click( function() { 
             setNick(); 
-            $( 'input' ).focus(); // focus on message input after submitting nick
+            $( '#messageInput' ).focus(); // focus on message input after submitting nick
         } );
 	    $( "#submit" ).click( function() { 
             sendMessage(); 
-            $( 'input' ).focus(); // focus on message input after submitting message
+            $( '#messageInput' ).focus(); // focus on message input after submitting message
         } );
     });
 
     // pressing enter sends message
-    $( 'input' ).keydown( function( event ) {
+    $( '#nickInput, #messageInput' ).keydown( function( event ) {
         if ( event.keyCode == 13 ) {
             $( this ).next( 'button' ).click();
         };
--- server/server.js
+++ server/server.js
@@ -7,47 +7,48 @@ var express = require('express'),
     server = http.createServer(app),
     io = require('socket.io').listen(server);
 
+var router = express.Router();
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.set("view options", { layout: false });
+app.set('view options', { layout: false });
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.send('ok');
-  console.log('someone connected on the front end')
+  console.log('\nFrom Server: Someone just connected to homepage.\n')
 })
 
-app.get('/:room', function(req, res) {
+app.get('/room/:roomId', function(req, res) {
+  var roomId = req.params.roomId;
   res.render('chat.jade');
-  console.log('someone just connected to the room: ' + req.params.room);
+  console.log('\nFrom Server: Someone just visited this room URL: "'+roomId+'".\n');
 })
 
 server.listen(appPort);
 
-io.configure(function () { 
+io.configure(function() { 
   io.set("transports", ["xhr-polling"]); 
   io.set("polling duration", 10); 
 });
 
 // do this when a new connection is made to the site
-io.sockets.on('connection', function (socket) {
-  console.log(socket.name + " has connected")
+io.sockets.on('connection', function(socket) {
+  console.log('\nFrom Server: Someone has just connected to a socket.\n')
   var roomId = "";
 
   socket.on('join', function(roomName){
     socket.join(roomName);
-    console.log(socket.name + ' has joined room: ' + roomName)
+    roomId = roomName;
+    console.log('\nFrom Server: Someone has joined room: "'+roomId+'".\n');
   });
 
-	socket.on('setNick', function (data) {
+	socket.on('setNick', function(data) {
     socket.set('nick', data);
 	});
 
-	socket.on('message', function (message) {
-		socket.get('nick', function (error, name) {
-			var data = { 'message' : message, nick : name };
-			socket.broadcast.to(roomId).emit('message', data);
-			console.log("user: " + name + "\n" + "sent this: " + message + "\n" + 'to room: "' + roomId + '"\n');
-		})
+	socket.on('message', function(messageObject) {
+    socket.broadcast.to(roomId).emit('message', messageObject);
+    console.log('\nFrom Server: \nUser: '+messageObject.nick+'\n' + 'sent this: "'+messageObject.message+'"\n' + 'to room: "'+roomId+'"\n');
 	});
 });
