0f14a36aae0d5a4858a75252cad439105e1d4f78
Danny Shekhtman <danielshekhtman@gmail.com>
2014-05-20 15:43:59 -0400

    buncha changes

--- extension/injectChat.js
+++ extension/injectChat.js
@@ -8,8 +8,8 @@ if(document.getElementById('chatboxFrame')){
 } else {
   var permachatIframe = document.createElement('iframe');
   permachatIframe.id = 'chatboxFrame';
-  permachatIframe.setAttribute('src','./chatbox.html');
-  permachatIframe.setAttribute('style','margin: 0px; border: 0px; position: fixed; display: block; border: none; z-index: 9999999; bottom: 0; left: 0; width: 100%; height: 200px');
+  permachatIframe.setAttribute('src','http://localhost:3000/room/dumb');
+  permachatIframe.setAttribute('style','margin: 0px; border: 0px; position: fixed; display: block; border: none; z-index: 9999999; bottom: 0; left: 0; width: 100%; height: 200px;');
   html.style.marginBottom = '200px';// this doesn't seem to work all the time -- see w3schools
   body.appendChild(permachatIframe);
 };
